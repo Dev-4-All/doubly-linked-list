ee924694e90bd8ae8b90861420248d63f22aa15c
Dev-4-All <devforall2005@gmail.com>
2026-08-29 10:05:14 +0530

    Add append functionality

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from doubly_linked_list import DoublyLinkedList
 
+# create
 my_doubly_linked_list = DoublyLinkedList(5)
+my_doubly_linked_list.print_list()
+
+# append
+my_doubly_linked_list.append(10)
 my_doubly_linked_list.print_list()
